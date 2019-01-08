907498d6b109a944b86a6057cbd5f75b7f4d547a
Brian Mottershead <bmotters@gmail.com>
2019-01-08 14:27:01 -0500

    Update build.sh

--- tools/build.sh
+++ tools/build.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 # Builds the QTI.JS package and test/preview files
 
-DIR=$(dirname $0)
-
-DOCROOT=$(realpath $DIR/../docroot)
-mkdir -p $DOCROOT
-
-cd $DIR/..
 rm -rf package
-mkdir -p package/qtijs
-cp LICENSE package/qtijs/LICENSE
-cp src/VERSION package/qtijs/VERSION
+mkdir -p package docroot
+cp LICENSE package/
+cp src/VERSION package/
 cp src/index.html package/
-$DIR/bundle.sh > package/qti.js
+tools/bundle.sh > package/qti.js
 cp -a themes/basic package/theme
-
 rm qtijs.package.zip -f
 (cd package; zip ../qtijs.package.zip -r ./ >/dev/null 2>&1)
-
-cd test
-touch dummy~; rm $(find . -name '*~')
-rsync -av -o -g -L --delete --exclude work ./ $DOCROOT/
+rsync -av -o -g -L  ./test/ ./docroot/
